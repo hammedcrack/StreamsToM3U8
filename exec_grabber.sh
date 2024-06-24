8b1dd66a73963c203492937814a1a4b865b0f10a
hammedcrack <149377986+hammedcrack@users.noreply.github.com>
2024-06-24 08:15:55 +0100

    Update exec_grabber.sh

--- exec_grabber.sh
+++ exec_grabber.sh
@@ -5,5 +5,8 @@ python3 -m pip install pytz
 python3 -m pip install beautifulsoup4
 
 python3 grabber.py > ./streams.m3u8
+rm ./index.m3u8
+wget --content-disposition --trust-server-names -i ./streams.m3u8
+rm ./no_stream_2.mp4
 
 echo M3U update complete.
